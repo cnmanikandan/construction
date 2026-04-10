17c7e29c7be647b36354970c88f1e6ce2554d112
Manikandan <maanikprince@gmail.com>
2026-04-10 06:56:36 +0000

    Add debug text to check rendering

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import NavBar from "./Component/NavBar";
 function App() {
   return (
     <BrowserRouter>
+      <h1 style={{color: 'black'}}>Hello World</h1>
       <NavBar />
     </BrowserRouter>
   );
