7639b590a795cbbcf0220a2b3301605d3af3961c
Manikandan <maanikprince@gmail.com>
2026-04-10 06:57:46 +0000

    Fix body styles to allow normal rendering

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import NavBar from "./Component/NavBar";
 function App() {
   return (
     <BrowserRouter>
-      <h1 style={{color: 'black'}}>Hello World</h1>
       <NavBar />
     </BrowserRouter>
   );
